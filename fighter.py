7e6ec9fdbd24b80dccdff6d9e98b24b012a60f18
Gabriele Lavorato <gb.lavorato@gmail.com>
2021-09-15 10:08:10 +0200

    Added figther class

---
+++ fighter.py
@@ -0,0 +1,13 @@
+# Object class for a fighter
+
+class Fighter:
+    def __init__(self, name, description, stamina, punchforce, guts, tecnique, nimble, tough, philosophy):
+        self.name = name
+        self.description = description
+        self.stamina = stamina
+        self.punchforce = punchforce
+        self.guts = guts
+        self.tecnique = tecnique
+        self.nimble = nimble
+        self.tough = tough
+        self.philosophy = philosophy
